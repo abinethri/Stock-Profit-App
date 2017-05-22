d253712af5bde8378aa0cb6fae4182242a437a14
Abinethri <abinethri.sreenaath@gmail.com>
2017-05-22 06:21:12 +0000

    Removing myvenv directory and all the .pyx files from git

--- myvenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\abinethri\desktop\stock-portfolio-suggestion\myvenv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
